26acc1d355966d9a1a8f8f3fe6bcca133159ac9c
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-04-26 18:11:18 +0200

    Corrected css file.

--- net.comes.care.ui/src/net/comes/care/ui/views/DataView.java
+++ net.comes.care.ui/src/net/comes/care/ui/views/DataView.java
@@ -3,6 +3,7 @@ package net.comes.care.ui.views;
 import javax.inject.Inject;
 
 import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 public class DataView {
@@ -13,7 +14,7 @@ public class DataView {
 	
 	@Inject
 	protected void createContent(Composite parent) {
-		dataViewer = new TreeViewer(parent);
+		dataViewer = new TreeViewer(parent, SWT.NONE);
 	}
 	
 }
