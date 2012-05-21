91f6c46f7c159b3857f28022e3f981558ead3308
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-05-21 14:19:49 +0200

    Modified sensor package.

--- net.comes.care.sensors/src/net/comes/care/sensors/SendsorAccelerometer.java
+++ net.comes.care.sensors/src/net/comes/care/sensors/SendsorAccelerometer.java
@@ -1,8 +1,9 @@
 package net.comes.care.sensors;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URI;
 
+import weka.core.Instances;
 import de.lmu.ifi.dbs.medmon.sensor.core.AbstractSensor;
 import de.lmu.ifi.dbs.medmon.sensor.core.IConverter;
 
@@ -28,13 +29,19 @@ public class SendsorAccelerometer extends AbstractSensor{
 		return ".sdr";
 	}
 
+
 	@Override
-	public boolean isConvertable(InputStream inputStream) throws IOException {
-		return false;
+	public Instances getHeader() {
+		return null;
 	}
 
 	@Override
-	public IConverter newConverter(InputStream inputStream) throws IOException {
+	public boolean isConvertable(URI input) throws IOException {
+		return true;
+	}
+
+	@Override
+	public IConverter newConverter(URI input) throws IOException {
 		return null;
 	}
 
