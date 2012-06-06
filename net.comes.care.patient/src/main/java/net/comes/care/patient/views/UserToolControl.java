9031f2431c69c88ea6e97f923db4dfffde9a38ad
Nepomuk Seiler <nepomuk.seiler@mukis.de>
2012-06-06 11:59:31 +0200

    Smooth login

--- net.comes.care.patient/src/main/java/net/comes/care/patient/views/UserToolControl.java
+++ net.comes.care.patient/src/main/java/net/comes/care/patient/views/UserToolControl.java
@@ -2,15 +2,17 @@ package net.comes.care.patient.views;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.xml.ws.soap.SOAPFaultException;
 
 import net.comes.care.common.login.SessionStore;
-import net.comes.care.common.login.UserPasswordDialog;
+import net.comes.care.ws.sycare.Credentials;
 import net.comes.care.ws.sycare.Session;
 import net.comes.care.ws.sycare.service.Sycare;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -28,19 +30,19 @@ public class UserToolControl {
 	private Link login;
 
 	@PostConstruct
-	public void createContent(Composite parent, final SessionStore store) {
+	public void createContent(final Composite parent, final SessionStore store) {
 		parent.setLayout(new GridLayout(4, false));
 		
-		final Label lblUsername = new Label(parent, SWT.NONE);
-		lblUsername.setText("Nicht eingeloggt");
-		lblUsername.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		final Label lblStatus = new Label(parent, SWT.NONE);
+		lblStatus.setText("Nicht eingeloggt");
+		lblStatus.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 		
 		txtUsername = new Text(parent, SWT.BORDER);
 		GridData layoutData = new GridData(SWT.RIGHT, SWT.CENTER, false, false);
 		layoutData.widthHint = 170;
 		txtUsername.setLayoutData(layoutData);
 		txtUsername.setToolTipText("Benutzername");
-		
+			
 		txtPassword = new Text(parent, SWT.BORDER | SWT.PASSWORD);
 		layoutData = new GridData(SWT.RIGHT, SWT.CENTER, false, false);
 		layoutData.widthHint = 170;
@@ -52,14 +54,36 @@ public class UserToolControl {
 		login.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				lblUsername.setText("Muki Seiler");
-				
-				UserPasswordDialog dialog = new UserPasswordDialog(txtUsername.getShell(), sycare);
-				if (dialog.open() == UserPasswordDialog.CANCEL)
+				//Already logged in
+				if(store.getSession().isPresent()) {
+					store.setSession(null);
+					login.setText("<a>login</a>");
+					txtUsername.setVisible(true);
+					txtPassword.setVisible(true);
+					parent.layout();
 					return;
+				}
+				
+				//Login
+				Credentials credentials = new Credentials();
+				credentials.setUsername(txtUsername.getText());
+				credentials.setPassword(txtPassword.getText());
+				
+				try {
+					Session session = sycare.authenticate(credentials);
+					store.setSession(session);
+					lblStatus.setText(txtUsername.getText());
+					login.setText("<a>ausloggen</a>");
+					txtUsername.setVisible(false);
+					txtUsername.setText("");
+					txtPassword.setVisible(false);
+					txtPassword.setText("");
+					parent.layout();
+				} catch (SOAPFaultException ex) {
+					ex.printStackTrace();
+					lblStatus.setText("Falscher Benutzername oder Passwort");
+				}
 
-				Session session = dialog.getSession();
-				store.setSession(session);
 			}
 		});
 		
